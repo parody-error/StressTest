a07aff49580d8c40bb040af78b52298fc330d14f
parody-error <armistad@gmail.com>
2025-04-17 15:35:56 -0600

    Adding initial views models and viewmodels

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace StressTest.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        public StressTestViewModel StressTestViewModel { get; } = new StressTestViewModel();
+    }
+}
---
+++ ViewModels/StressTestViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Commands;
+using System;
+
+namespace StressTest.ViewModels
+{
+    internal class StressTestViewModel : ViewModelBase
+    {
+        public DelegateCommand AllocateCommand { get; }
+        private bool CanExecuteAllocateCommand => true; //#SB: make sure we have a non-negative memory specified.
+
+        public StressTestViewModel()
+        {
+            AllocateCommand = new DelegateCommand(
+                ExecuteAllocateCommand,
+                () => CanExecuteAllocateCommand
+            );
+        }
+
+        private void ExecuteAllocateCommand()
+        {
+            if ( !CanExecuteAllocateCommand )
+                return;
+
+            Console.WriteLine( "Allocating" );
+        }
+    }
+}
