5864a8bab4ba21c467fe68bcfd5f884f6b72ca7d
parody-error <armistad@gmail.com>
2025-04-17 16:07:24 -0600

    Adding Query button

--- ViewModels/StressTestViewModel.cs
+++ ViewModels/StressTestViewModel.cs
@@ -5,14 +5,33 @@ namespace StressTest.ViewModels
 {
     internal class StressTestViewModel : ViewModelBase
     {
+        private string _query = "SELECT * FROM WELL";
+        public string Query
+        {
+            get => _query;
+            set
+            {
+                _query = value;
+                QueryCommand.RaiseCanExecuteChanged();
+            }
+        }
+
         public DelegateCommand AllocateCommand { get; }
         private bool CanExecuteAllocateCommand => true; //#SB: make sure we have a non-negative memory specified.
+
+        public DelegateCommand QueryCommand { get; }
+        private bool CanExecuteQueryCommand => !string.IsNullOrEmpty( Query );
 
         public StressTestViewModel()
         {
             AllocateCommand = new DelegateCommand(
                 ExecuteAllocateCommand,
                 () => CanExecuteAllocateCommand
+            );
+
+            QueryCommand = new DelegateCommand(
+                ExecuteQueryCommand,
+                () => CanExecuteQueryCommand
             );
         }
 
@@ -22,6 +41,14 @@ namespace StressTest.ViewModels
                 return;
 
             Console.WriteLine( "Allocating" );
+        }
+
+        private void ExecuteQueryCommand()
+        {
+            if ( !CanExecuteQueryCommand )
+                return;
+
+            Console.WriteLine( "Querying" );
         }
     }
 }
